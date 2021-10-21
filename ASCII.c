8efb7111beaab876d61b2069e179b3fe5a4e3aed
rohith <rohith3085@gmail.com>
2021-10-21 04:47:30 +0000

    To find the ASCII value

---
+++ ASCII.c
@@ -0,0 +1,10 @@
+// To print the ASCII value of the character 
+#include <stdio.h>
+int main() {  
+    char Asc;
+    scanf("%c", &Asc);  
+    // %d displays the integer value of a character
+    printf("%d\n",Asc);
+    
+    return 0;
+}
