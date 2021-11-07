a74476286cdc894fa140703da83d6d89c16abe47
rohith <rohith3085@gmail.com>
2021-11-07 07:23:21 +0000

    To find the speed difference

---
+++ speed.c
@@ -0,0 +1,15 @@
+// Aray and Aaron are participating in the Bike racing. Aray have crossed some milestores 
+// earlier and Aaron crossed some milestores earlier during their racing because they have changed 
+// their speeds at different times,Both of them like to know the difference in speeds between them 
+// at different stages of racing Can you help finding the speed difference between Aray and Aaron?
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int aravspeed,aaronspeed,speeddiff;
+    scanf("%d",&aravspeed);
+    scanf("%d",&aaronspeed);
+    if(speeddiff){
+        speeddiff=aravspeed-aaronspeed;
+        printf("%d",speeddiff);}
+    return 0;
+}
